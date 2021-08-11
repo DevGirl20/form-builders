49c85c985e37e4bc10777254edd973997d39d59b
DevGirl20 <karina.kolesnik.1998@gmail.com>
2021-08-11 18:29:16 +0300

    2.2 commit

--- forms-builder/src/app/accordion/accordion-item.component.ts
+++ forms-builder/src/app/accordion/accordion-item.component.ts
@@ -1,9 +1,25 @@
+import { state, transition } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
+import {trigger, style, animate} from '@angular/animations'
 
 @Component({
   selector: 'app-accordion-item',
   templateUrl: './accordion-item.component.html',
-  styleUrls: ['./accordion-item.component.css']
+  styleUrls: ['./accordion-item.component.css'], 
+  animations:[
+    trigger('smoothCollapse', [
+      state('initial', style({
+height: '0',
+overflow: 'hidden', 
+opacity:'0', 
+visibility: 'hidden'
+      })),
+      state('final', style({
+        overflow: 'hidden'
+      })), 
+      transition('initial<=>final', animate('300ms'))
+    ])
+  ]
 })
 export class AccordionItemComponent implements OnInit {
 
